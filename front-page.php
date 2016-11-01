46dcdc4b01eb93b4ab59d33799c74e90c0c13711
Meagan Pau <meagan.pau@gmail.com>
2016-11-01 19:04:56 -0400

    header stylin

--- front-page.php
+++ front-page.php
@@ -6,11 +6,13 @@
 get_header();  ?>
 <?php $heroImage = get_field('header-hero') ?>
 <header class="hero" style="background-image:url('<?php echo $heroImage['url']; ?>')">
-    <?php $headerLogo = get_field('header-logo'); ?>
-    <img src="<?php echo $headerLogo['url'] ?>" alt="" />
-    <h1><?php the_field('header-title') ?></h1>
-    <h2><?php the_field('header-tagline') ?></h2>
-    <a href="<?php the_field('header-link'); ?>"><?php the_field('header-link-text'); ?></a>
+    <div class="hero-content">
+      <?php $headerLogo = get_field('header-logo'); ?>
+      <img src="<?php echo $headerLogo['url'] ?>" alt="" />
+      <h1><?php the_field('header-title') ?></h1>
+      <h2><?php the_field('header-tagline') ?></h2>
+      <a class="btn___header" href="<?php the_field('header-link'); ?>"><?php the_field('header-link-text'); ?></a>
+    </div>
 </header>
 
 <?php get_footer(); ?>
