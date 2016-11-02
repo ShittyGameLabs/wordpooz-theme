f1bc703f6cb969c4ec4276658fe8547169cd895a
Will DiFruscio <wdifruscio@gmail.com>
2016-11-02 14:45:36 -0400

    fail

--- front-page.php
+++ front-page.php
@@ -16,22 +16,58 @@ get_header();  ?>
 </header>
 <section class="about" id="about">
   <div class="wrapper">
-    <h1><?php the_field('about-title'); ?></h1>
-    <h2><?php the_field('about-tagline'); ?></h2>
-    <div class="about___icons">
-    <?php
-      while(have_rows('about-icons')) : the_row();
-      ?>
-        <div class="about___icons---box">
-          <?php $imageUrl = get_sub_field('about-icon-image'); ?>
-          <img src="<?php echo $imageUrl['url'] ?>" alt="" />
-          <h3><?php the_sub_field('about-icon-title'); ?></h3>
-          <p>
-            <?php the_sub_field('about-icon-content'); ?>
-          </p>
-        </div>
-    <?php endwhile; ?>
+      <h1><?php the_field('about-title'); ?></h1>
+      <h2><?php the_field('about-tagline'); ?></h2>
+        <div class="about___icons">
+      <?php
+        while(have_rows('about-icons')) : the_row();
+        ?>
+          <div class="about___icons---box">
+            <?php $imageUrl = get_sub_field('about-icon-image'); ?>
+            <img src="<?php echo $imageUrl['url'] ?>" alt="" />
+            <h3><?php the_sub_field('about-icon-title'); ?></h3>
+            <p>
+              <?php the_sub_field('about-icon-content'); ?>
+            </p>
+          </div>
+      <?php endwhile; ?>
+    </div>
   </div>
+</section>
+<section class="projects" id="projects">
+  <div class="wrapper">
+    <h1><?php the_field('project-title') ?></h1>
+    <h2><?php the_field('project-tagline') ?></h2>
+      <div class="project-wrapper">
+        <div class="figure-container">
+        <?php
+        while(have_rows('project-box')) : the_row();
+        ?>
+            <figure>
+              <?php $projectImage = get_sub_field('project-image'); ?>
+              <img src="<?php echo $projectImage['url'] ?>" alt="">
+            </figure>
+            <?php endwhile; ?>
+        </div>
+        <div class="figure-container-2">
+            <?php while(have_rows('project-box-two')) : the_row();
+            ?>
+            <figure>
+              <?php $projectImage = get_sub_field('project-image-two'); ?>
+              <img src="<?php echo $projectImage['url'] ?>" alt="">
+            </figure>                 
+            <?php endwhile; ?>  
+        </div>
+        <div class="figure-container-3">
+            <?php while(have_rows('project-box-three')) : the_row();
+            ?>
+            <figure>
+              <?php $projectImage = get_sub_field('project-image-three'); ?>
+              <img src="<?php echo $projectImage['url'] ?>" alt="">
+            </figure>                 
+            <?php endwhile; ?>  
+        </div>
+      </div>
   </div>
 </section>
 
