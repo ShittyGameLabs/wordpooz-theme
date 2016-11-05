bf46d9849a4f6fece1f695b849b63a205a5efa98
Will DiFruscio <wdifruscio@gmail.com>
2016-11-05 13:27:37 -0400

    added post page styling

--- single.php
+++ single.php
@@ -2,15 +2,16 @@
 	Template Name: Single Blog Post
 */-->
 
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <?php get_header(); ?>
-<?php $blogImage = the_sub_field('blog-header-image') ?>
-<header class='post___header' id="nav___anchor" style="background-image:linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)),url(<?php $blogImage ?>)">
-  <h1>Blog</h1>
+<header class='blog___header' id="nav___anchor" style="background-image:linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)),url(<?php the_field('post-header-image') ?>)">
+    <hgroup class="wrapper">
+      <h1>Blog</h1>
+    </hgroup>
 </header>
-<div class="main">
+<div class="main blog-content">
   <div class="container">
     <div class="content">
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <h1 class="entry-title"><?php the_title(); ?></h1>
           <div class="entry-meta">
